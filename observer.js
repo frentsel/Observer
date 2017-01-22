ecc7943da033460b61760477d62df88820bfe718
Alex Shulz <frentsel@mail.ru>
2017-01-22 14:56:22 +0200

    init

---
+++ observer.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var observer = new function(){
+
+	var listeners = [];
+
+	this.subscribe = function(e, handler){
+		if(!listeners[e]) listeners[e]= [];
+		listeners[e].push(handler);
+	};
+
+	this.unSubscribe = function(e, handler){
+
+		if(!listeners[e]) return false;
+		if(!handler) return delete listeners[e];
+
+		listeners[e].map(function (_handler, n) {
+			if(handler === _handler) delete listeners[e][n];
+		});
+	};
+
+	this.publish = function(e, args){
+		if(!listeners[e]) return false;
+		listeners[e].map(function (handler) {
+			handler(args);
+		});
+	};
+};
