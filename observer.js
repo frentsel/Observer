05020c7bebeb044ff12a89ab27a58cb0105a8eeb
Alex Shulz <frentsel@mail.ru>
2017-01-22 15:36:35 +0200

    whitespaces

--- observer.js
+++ observer.js
@@ -1,24 +1,16 @@
-'use strict';
-
 var Observer = function(){
-
 	var listeners = [];
-
 	this.subscribe = function(e, handler){
 		if(!listeners[e]) listeners[e]= [];
 		listeners[e].push(handler);
 	};
-
 	this.unsubscribe = function(e, handler){
-
 		if(!listeners[e]) return false;
 		if(!handler) return delete listeners[e];
-
 		listeners[e].map(function (_handler, n) {
 			if(handler === _handler) delete listeners[e][n];
 		});
 	};
-
 	this.publish = function(e, args){
 		if(!listeners[e]) return false;
 		listeners[e].map(function (handler) {
