b0368147e566e0a1352537c7a69b13c97707fd7c
Frentsel Alexandr <frentsel@mail.ru>
2018-07-26 13:26:04 +0300

    Updated observer.js
    Added opportunity to pass multiple params to the publish function.

--- observer.js
+++ observer.js
@@ -1,20 +1,28 @@
-var Observer = function(){
-	var listeners = {};
-	this.subscribe = function(e, handler){
-		if(!listeners[e]) listeners[e] = [];
-		listeners[e].push(handler);
-	};
-	this.unsubscribe = function(e, handler){
-		if(!listeners[e]) return false;
-		if(!handler) return delete listeners[e];
-		listeners[e].map(function (_handler, n) {
-			if(handler === _handler) delete listeners[e][n];
-		});
-	};
-	this.publish = function(e, args){
-		if(!listeners[e]) return false;
-		listeners[e].map(function (handler) {
-			handler(args);
-		});
-	};
+var Observer = function() {
+
+  var listeners = {};
+
+  this.subscribe = (e, handler) => {
+    !listeners[e] && (listeners[e] = []);
+    listeners[e].push(handler);
+  };
+
+  this.unsubscribe = (e, handler) => {
+
+    if (!listeners[e])
+      return false;
+
+    if (!handler)
+      return delete listeners[e];
+
+    listeners[e].map((_handler, n) => handler === _handler && delete listeners[e][n]);
+  };
+
+  this.publish = (e, ...args) => {
+    if (listeners[e]) {
+      listeners[e].map((handler) => {
+        handler.apply(null, args);
+      });
+    }
+  };
 };
